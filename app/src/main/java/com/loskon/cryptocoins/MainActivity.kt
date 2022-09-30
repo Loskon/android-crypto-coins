91106600e88c08345578353e5467ecb5b07d762e
Loskon <andreyrochev23@gmail.com>
2022-09-30 22:10:46 +0300

    Initial commit

---
+++ app/src/main/java/com/loskon/cryptocoins/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.loskon.cryptocoins
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
