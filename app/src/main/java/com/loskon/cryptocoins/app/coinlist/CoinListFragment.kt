9df08da21b3eaf5f85ff924b795eb201ef6a7a20
Loskon <andreyrochev23@gmail.com>
2022-09-30 23:29:20 +0300

    Created a main layout with a list of cryptocurrencies

---
+++ app/src/main/java/com/loskon/cryptocoins/app/coinlist/CoinListFragment.kt
@@ -0,0 +1,7 @@
+package com.loskon.cryptocoins.app.coinlist
+
+import androidx.fragment.app.Fragment
+import com.loskon.cryptocoins.R
+
+class CoinListFragment: Fragment(R.layout.fragment_coin_list) {
+}
