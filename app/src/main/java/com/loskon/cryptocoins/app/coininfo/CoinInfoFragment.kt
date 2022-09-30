7450140d3bc110a0858b70a19386074b740719a2
Loskon <andreyrochev23@gmail.com>
2022-10-01 00:27:10 +0300

    Created a layout with detailed info about cryptocurrency

---
+++ app/src/main/java/com/loskon/cryptocoins/app/coininfo/CoinInfoFragment.kt
@@ -0,0 +1,7 @@
+package com.loskon.cryptocoins.app.coininfo
+
+import androidx.fragment.app.Fragment
+import com.loskon.cryptocoins.R
+
+class CoinInfoFragment : Fragment(R.layout.fragment_coin_info) {
+}
